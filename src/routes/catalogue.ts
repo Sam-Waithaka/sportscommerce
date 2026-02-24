07a1ccb28dd2cc1d3a54f785530def0d4ba5b2d1
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-24 08:30:11 +0300

    feat: update server configuration and add Handlebars templates

--- src/routes/catalogue.ts
+++ src/routes/catalogue.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 
 export const createCatalogRoutes = (app: Express)=>{
     app.get('/', (req, res)=>{
-        res.send("Hello sportsstore route")
+        // res.send("Hello sportsstore route")
+        res.render('index')
     })
 }
