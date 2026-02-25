956755fbdfeecbe2b3bf1630042a441d2bac1596
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-25 18:33:56 +0300

    feat: add catalog models and repository for product management
    - Added Product, Category, and Supplier interfaces in catalog_models.ts.
    - Implemented CatalogRepository interface in catalog_repository.ts for CRUD operations.
    - Created catalog_helpers.ts to initialize Sequelize models and define relationships.
    - Defined ProductModel, CategoryModel, and SupplierModel in catalog_models.ts with Sequelize.
    - Exported models and initialization function from index.ts for easier access.
    - Updated package.json to include sequelize and sqlite3 dependencies.

---
+++ src/data/orm/models/catalog_helpers.ts
@@ -0,0 +1,34 @@
+import { DataType, DataTypes, Sequelize } from "sequelize";
+import { CategoryModel, ProductModel, SupplierModel } from "./catalog_models";
+
+const primaryKey ={
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+}
+
+export const initializeCatalogModels = (sequelize: Sequelize)=>{
+    ProductModel.init ({
+        ... primaryKey,
+        name: {type: DataTypes.STRING},
+        description: {type: DataTypes.STRING},
+        price: {type: DataTypes.DECIMAL(10, 2)}
+    }, {sequelize})
+
+    CategoryModel.init({
+        ...primaryKey,
+        name: {type: DataTypes.STRING}
+    }, {sequelize})
+
+    SupplierModel.init({
+        ...primaryKey,
+        name: {type: DataTypes.STRING}
+    }, {sequelize})
+
+    ProductModel.belongsTo(CategoryModel, {foreignKey: 'categoryId', as: 'category'})
+    ProductModel.belongsTo(SupplierModel, {foreignKey: 'supplerid', as: 'supplier'})
+    CategoryModel.hasMany(ProductModel, {foreignKey: 'categoryId', as: 'products'})
+    SupplierModel.hasMany(ProductModel, {foreignKey: 'supplierId', as: 'products'})
+}
